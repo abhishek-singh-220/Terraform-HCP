5b7772102515004f4b01e368bd9ef1b3ecd70033
abhishek-singh-220 <abhishekrajput220@gmail.com>
2026-01-03 10:32:10 +1100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,10 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
 variable "region"{
- type = map
+ type = map(string)
  default = {
    ap-southeast = "ami-00392df720ebc45d1"
    us-east-1 = "ami-000750470f5687d65"
@@ -8,7 +13,7 @@ variable "region"{
 
 
 resource "aws_instance" "myec2" {
-  ami           = var.region[var.region]
+  ami           = var.region[var.aws_region]
   instance_type = "t3.micro"
 
   tags = {
