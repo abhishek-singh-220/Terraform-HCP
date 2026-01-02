29e6c568c14cfeb22717512f888ce2ad5ca666eb
abhishek-singh-220 <abhishekrajput220@gmail.com>
2026-01-03 10:20:12 +1100

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+variable "region"{
+ type = map
+ default = {
+   ap-southeast = "ami-00392df720ebc45d1"
+   us-east-1 = "ami-000750470f5687d65"
+ }
+}
+
+
+resource "aws_instance" "myec2" {
+  ami           = var.region
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "FirstEc2Instance"
+  }
+}
